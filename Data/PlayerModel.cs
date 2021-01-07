348648c2b20cda19b7daa3bc07b4537ea5823327
Kisan <kisan@sysapp.ca>
2021-01-07 11:57:35 -0500

    Add project files.

---
+++ Data/PlayerModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NCCC_Blazor.Data
+{
+    public class PlayerModel
+    {
+        public string ID { get; set; }
+        public string PlayerName { get; set; }
+        public string Role { get; set; }
+        public string Ball { get; set; }
+        public string Bat { get; set; }
+        public string Image_file { get; set; }
+        public string Club { get; set; }
+        public string Team { get; set; }
+        public BattingRecords Batting { get; set; }
+        public BowlingRecords Bowling { get; set; }
+        public FieldingRecords Fielding { get; set; }
+
+    }
+    public class PlayerList
+    {
+        public PlayerModel[] PlayerModel { get; set; }
+    }
+
+    public class BattingRecords
+    {
+        public int Matches { get; set; }
+        public int Innings { get; set; }
+        public int RunScored { get; set; }
+        public int Fours { get; set; }
+        public int Sixs { get; set; }
+        public int StrikeRate { get; set; }
+        public int Fifties { get; set; }
+        public int Hundred { get; set; }
+    }
+
+    public class BowlingRecords
+    {
+        public int Matches { get; set; }
+        public int Innings { get; set; }
+        public int Balls { get; set; }
+        public int FiveWicket { get; set; }
+        public int Runs { get; set; }
+        public int Maidens { get; set; }
+        public int Wicket { get; set; }
+        public int Economy { get; set; }
+    }
+
+    public class FieldingRecords
+    {
+        public int Matches { get; set; }
+        public int Innings { get; set; }
+        public int Catches { get; set; }
+        public int RanOut { get; set; }
+        public int Stump { get; set; }
+    }
+}
---
+++ Data/PlayerModelService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace NCCC_Blazor.Data
+{
+    public class PlayerModelService
+    {
+        //public List<PlayerModel> player;
+        public Task<PlayerList[]> GetPlayers()
+        {
+            string json = System.IO.File.ReadAllText("C:/Users/kisan/source/repos/NCCC-Blazor/Data/PlayersList.json");
+            
+            PlayerList player = Newtonsoft.Json.JsonConvert.DeserializeObject<PlayerList>(json);
+            
+            //PlayerModel userCopy = System.Text.Json.JsonSerializer.Deserialize<PlayerModel>(json);
+
+            return Task.FromResult(Enumerable.Range(1, player.PlayerModel.Count()).Select(index => new PlayerList
+            {
+                PlayerModel = player.PlayerModel
+            }).ToArray());
+
+        }
+
+    }
+}
